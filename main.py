76a69413c7ce1fdd6e3039d521a942094869e825
Albertlll <king.python@mail.ru>
2023-12-05 13:04:51 +0300

    Высокое разрешение экранов

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 from main_window import MainForm
+from PyQt5 import QtWidgets, QtCore
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtGui import QGuiApplication
 from PyQt5.QtCore import Qt
@@ -10,6 +11,19 @@ def except_hook(cls, exception, traceback):
     sys.__excepthook__(cls, exception, traceback)
 
 
+
+
+if hasattr(QtCore.Qt, 'AA_EnableHighDpiScaling'):
+
+    QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
+
+
+
+if hasattr(QtCore.Qt, 'AA_UseHighDpiPixmaps'):
+
+    QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps, True)
+
+    
 if __name__ == '__main__':
     os.environ["QT_AUTO_SCREEN_SCALE_FACTOR"] = "1"
     app = QApplication(sys.argv)
